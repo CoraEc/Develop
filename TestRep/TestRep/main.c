4f62c7e6c93a18021d9ea01d56ea0a986a80a510
Student 112 <student112@Antons-iMac-12.local>
2015-02-17 19:54:00 +0200

    Initial Commit

---
+++ TestRep/TestRep/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestRep
+//
+//  Created by Student 112 on 2/17/15.
+//  Copyright (c) 2015 Student 112. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
